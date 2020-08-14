e9313501e9313094c78ee755c2f71ce3a3854235
LAKSHAYK0804 <spambotofficial44@gmail.com>
2020-08-14 19:49:11 +0530

    customized

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -5,87 +5,82 @@ class Strings {
 
   // home:----------------------------------------------------------------------
   static const String portfolio = 'Portfolio';
-  static const String this_web = 'Yes this website uses f***ing ';
+  static const String this_web = 'Yes this website uses b***dy ';
   static const String cookies = 'cookies';
-  static const String in_flutter = 'in flutter';
+  static const String in_flutter = 'by Lakshay Kalra';
   static const String heart = ' ❤ ';
   static const String made_with = 'Made with';
 
   // intro:---------------------------------------------------------------------
   static const String hello = 'Hello';
-  static const String i_am_zubair = '\nI am\nZubair';
+  static const String i_am_zubair = '\nI am\nLakshay';
   static const String design = 'DES_\nIGN';
-  static const String mobile_dev = 'Mobile App Developer\nbased in Islamabad\nPakistan.';
+  static const String mobile_dev = 'Coder and designer\nCEO of CtrlAltCompete';
 
   // about:---------------------------------------------------------------------
   static const String about_me = 'About Me';
   static const String about = 'Ab_\nout';
   static const String me = ' Me';
-  static const String headline = 'I\'am Zubair Rehman, Mobile App Developer from Islamabad, Pakistan';
-  static const String summary = 'Focused professional having excellent technical and communication skills, and offering 6 years of experience in Computer industry. Proficient at designing and formulating test automation frameworks, writing code in various languages, feature development and implementation. Specialize in thinking outside the box to find unique solutions to difficult engineering problems.';
+  static const String headline = 'I\'am Lakshay Kalra from New Delhi.';
+  static const String summary = 'Part of the Syntax team at DPS International, Head of CtrlAltCompete and a passionate coder.';
 
   // skills:--------------------------------------------------------------------
   static const String skills = 'Sk_\nills';
   static const String experience = 'Experience';
   static const String skills_i_have = 'What Skills I Have';
-  static const String rights_reserved = '© 2019 IOTECK SOLUTIONS. ALL RIGHTS RESERVED.';
+  static const String rights_reserved = '';
 
   static final skillsList = [
-    'Java',
-    'Kotlin',
-    'Dart',
-    'Flutter',
-    'Android',
-    'iOS',
-    'Xamarin',
-    'Reactive Programming',
-    'Jenkins',
-    'Photoshop',
-    'JFrog Atrtifactory',
-    'Code Magic',
+    'JavaScript',
+    'JQuery',
+    'HTML',
+    'CSS',
+    'Python',
+    'Django',
+    'C++',
   ];
 
   static final educationList = [
     Education(
-      'Apr 2018',
+      'Mar 2021',
       'Present',
-      'Embrace-it Pakistan',
-      'Sr. Software Engineer',
+      'DPS International Saket',
+      'Student',
     ),
     Education(
-      'Apr 2016',
-      'Apr 2018',
-      'TEO International',
-      'Sr. Software Engineer',
+      'IIT Hackthon',
+      'Apr 2019',
+      '',
+      'Virtual reality app creator',
     ),
     Education(
-      'July 2014',
-      'March 2016',
-      'Citrusbits',
-      'Software Engineer',
+      'Xino 2020 Participant',
+      'August 2020',
+      'Hunt x Hackathon',
+      'Shortlisted',
     ),
   ];
 
   // hire:----------------------------------------------------------------------
   static const String hire = 'HIRE';
-  static const String always_interested = 'I\'m always interested about cool stuff. Are you minding a project?\n';
+  static const String always_interested = 'Working on something? Want help?\n';
   static const String lets_talk = 'Let\'s talk.';
 
 
   // menu items:----------------------------------------------------------------
-  static const String menu_facebook = 'FACEBOOK';
+  static const String menu_facebook = 'DISCORD';
   static const String menu_twitter = 'TWITTER';
   static const String menu_linked_in = 'LINKEDIN';
-  static const String menu_medium = ' MEDIUM ';
+  static const String menu_medium = 'WEBSITE';
   static const String menu_github = 'GITHUB';
-  static const String menu_mail = 'MAIL';
+  static const String menu_mail = 'WEBSITE';
 
   // links:---------------------------------------------------------------------
-  static const String menu_facebook_link = 'https://www.facebook.com/zubair340';
-  static const String menu_twitter_link = 'https://twitter.com/zubair340';
-  static const String menu_linked_in_link = 'https://www.linkedin.com/in/zubairehman/';
-  static const String menu_medium_link = 'https://medium.com/@zubairehman.work';
-  static const String menu_github_link = 'https://github.com/zubairehman';
-  static const String menu_mail_link = 'mailto:zubairehman.work@gmail.com';
+  static const String menu_facebook_link = 'https://discord.gg/RcgrREf';
+  static const String menu_twitter_link = 'https://twitter.com/lakshay_smh';
+  static const String menu_linked_in_link = 'https://www.linkedin.com/in/lakshay0804/';
+  static const String menu_medium_link = 'https://ctrlaltcompete.org';
+  static const String menu_github_link = 'https://github.com/LAKSHAYK0804';
+  static const String menu_mail_link = 'https://ctrlaltcompete.org';
 
 }
